3fa964864c22b1f4a0b85b1a082996b5cf1328a0
Naozumi520 <Nightcore1186@gmail.com>
2020-09-26 15:08:19 +0800

    test2

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,7 @@ const config = require('../config.json');
 
 module.exports.run = async (client, message, args) => {
     const m = await message.channel.send("Ping....");
-    m.edit(`Pong, Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+    m.edit(`Pong, Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms.`);
 }
 
 module.exports.config = {
