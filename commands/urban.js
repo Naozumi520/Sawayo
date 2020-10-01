caf6921fa1032475ca68b9b2fc11206486f6a418
cool.xaml <68362244+JammieLannie@users.noreply.github.com>
2020-10-01 11:29:51 +0700

    Add files via upload

---
+++ commands/urban.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const urban = require("urban");
+
+module.exports.run = async (bot, message, args) => {
+    if(args.length < 1) return message.reply("Please enter something!");
+    let word = args.join(" "); 
+
+
+
+    urban(word).first(json => {
+        if(!json) return message.reply("No results found!")
+        var definition = json.definition;
+        var length = 1024
+        let urbEmbed = new Discord.MessageEmbed()
+        .setColor("00ff00")
+        .setTitle(json.word)
+        .setDescription(definition.substring(0, length))
+        .addField("Upvotes", json.thumbs_up, true)
+        .addField("Downvotes", json.thumbs_down, true)
+        .setFooter(`Written by: ${json.author}`);
+
+        message.channel.send(urbEmbed)
+    });
+
+
+}
+
+module.exports.config = {
+    name: "urban"
+}
