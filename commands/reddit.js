0be0dc6c639bc4503bd188fa8d1f128b29acba65
Naozumi520 <Nightcore1186@gmail.com>
2020-10-05 14:56:33 +0800

    package.json GPL-3.0, reddit bigger loading icon
    i told u i hate description.

--- commands/reddit.js
+++ commands/reddit.js
@@ -5,16 +5,19 @@ module.exports.run = async (bot, message, args)  => {
         if(!Subreddit)
         return message.channel.send(`<:error:759791498279125042> You did not specify your subreddit!`)
         try{
-            var m = await message.channel.send("Loading random pic.... <a:loading:762232493834764308>");
+            var m = await message.channel.send("Loading random pic....")
+            var e = await message.channel.send("<a:loading:762232493834764308>")
             let img = await api(Subreddit)
             const Embed = new discord.MessageEmbed()
             .setTitle(`A random image from r/${Subreddit}`)
             .setColor('2f3136')
             .setImage(img)
             .setURL(`https://reddit.com/r/${Subreddit}`)
+            e.delete()
             m.edit('', Embed) //keep '',
         } catch(err) {
-            m.edit(`<:error:759791498279125042> An error occurred while loading the current SBR: \`${args}\``, err)
+            e.delete()
+            m.edit(`> <:error:759791498279125042> An error occurred while loading the current SBR: \`${args}\``, err)
         }   
     }
 module.exports.config = {
